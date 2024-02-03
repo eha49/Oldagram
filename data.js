e0cc0af9fda4720c93485f2de1d3313b5b4afcab
eha49 <habron.rasheed7@gmail.com>
2024-02-03 19:16:53 +0530

    Almost completed...requires some changes

--- data.js
+++ data.js
@@ -4,27 +4,33 @@ const posts = [
         username: "vincey1853",
         location: "Zundert, Netherlands",
         avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
+        postImage: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 216
+        likes: 216,
+        isLiked: false,
+        id: "1"
     },
     {
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
         avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
+        postImage: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 402
+        likes: 402,
+        isLiked: false,
+        id: "2"
     },
         {
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
         avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
+        postImage: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        isLiked: false,
+        id: "3"
     }
 ]
 
--- index.js
+++ index.js
@@ -2,25 +2,79 @@ import posts from "./data.js"
 
 const postsSection = document.getElementById('posts-section');
 
-const htmlString =
-    `
-    <section class='post'>
-        <div class='post-heading'>
-            <img src=${posts[0].avatar} class='post-author-avatar'>
-            <div class='post-author-info'>
-                <p class='post-author-name'>${posts[0].name}</p>
-                <p class='post-author-location'>${posts[0].location}</p>
-            </div>
-        </div>
-        <img src='${posts[0].post}' class='post-img'>
-        <div class='post-reactions'>
-            <img src='./images/icon-heart.png' class='post-icons heart-icon'>
-            <img src='./images/icon-comment.png' class='post-icons'>
-            <img src='./images/icon-dm.png' class='post-icons'> 
-        </div>
-        <p class='post-likes'>${posts[0].likes} likes</p>
-        <p class='post-caption'><span class='post-author-username'>${posts[0].username}</span> ${posts[0].comment}</p>
-    </section>
-    `
+function render() {
+    const htmlString = posts.map(post => {
+        return `
+            <section class='post' id='post'>
+                <div class='post-heading'>
+                    <img
+                        src=${post.avatar} 
+                        class='post-author-avatar'
+                    >
+                    <div class='post-author-info'>
+                        <p class='post-author-name'>
+                            ${post.name}
+                        </p>
+                        <p class='post-author-location'>
+                            ${post.location}
+                        </p>
+                    </div>
+                </div>
+                <img
+                    src='${post.postImage}' 
+                    class='post-img'
+                    data-likes='${post.id}'
+                >
+                <div class='post-reactions'>
+                    <img
+                        src='./images/icon-heart.png'
+                        class='post-icons heart-icon'
+                        data-likes='${post.id}'
+                    >
+                    <img 
+                        src='./images/icon-comment.png' 
+                        class='post-icons'
+                    >
+                    <img 
+                        src='./images/icon-dm.png' 
+                        class='post-icons'
+                    > 
+                </div>
+                <p class='post-likes'>
+                    ${post.likes} likes
+                </p>
+                <p class='post-caption'>
+                    <span class='post-author-username'>
+                        ${post.username}
+                    </span>
+                    ${post.comment}
+                </p>
+            </section>
+            `
+    }).join('');
+   
+    postsSection.innerHTML = htmlString;
+};
 
-    postsSection.innerHTML = htmlString
+function handleLikes(e) {
+    if (e.target.dataset.likes) {
+        updateLikes(e.target.dataset.likes);
+    }
+};
+
+function updateLikes(postId) {
+    const targetPost = posts.filter(post => {
+        return postId === post.id;
+    })[0];
+
+    !targetPost.isLiked ? targetPost.likes++ : targetPost.likes--;
+    targetPost.isLiked = !targetPost.isLiked;
+    render();
+    
+};
+
+render();
+
+
+
+postsSection.addEventListener('click', handleLikes);
